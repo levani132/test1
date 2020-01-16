92fc85e1635e15cdea391c6b0ae2f27680cf4362
Igel <gelenidze.irakli@Gmail.com>
2020-01-17 02:08:53 +0400

    212

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
-
+port = int(os.environ.get('PORT', 17995)) 
 @app.route('/', methods=['GET']) 
 def foo():
     data = {"test":23} #request.json
     return jsonify(data)
 
 #app.run(host="0.0.0.0", port=5000)
-app.run()
+app.run( port=port)
 
 #test = {"test_key":1000}
 #print (jsonify(test)) 
