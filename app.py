37600ba37f0014fd1119b80e42a6cda9e28631b3
Igel <gelenidze.irakli@Gmail.com>
2020-01-17 02:19:27 +0400

    1

--- app.py
+++ app.py
@@ -7,9 +7,8 @@ port = int(os.environ.get('PORT', 17995))
 def foo():
     data = {"test":23} #request.json
     return jsonify(data)
-
-#app.run(host="0.0.0.0", port=5000)
-app.run( port=port)
+app.run(host="0.0.0.0", port=5000)
+#app.run( port=port)
 
 #test = {"test_key":1000}
 #print (jsonify(test)) 
