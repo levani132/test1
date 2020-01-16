8f7d9e1e53c0f6856891b2bf3b74cd5e3b8efc62
Igel <gelenidze.irakli@Gmail.com>
2020-01-17 00:44:49 +0400

    6

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
-import flask
-app = flask.Flask(__name__)
+from flask import Flask, request, jsonify
 
-@app.route("/")
-def index():
-    response_value_1=1
-    response_value_2="Test"
-    return flask.jsonify(key1=response_value_1,key2=response_value_2)
+app = Flask(__name__)
 
-app.run() 
+@app.route('/', methods=['GET']) 
+def foo():
+    data = {"test":23} #request.json
+    return jsonify(data)
+
+app.run()
 
 
 #test = {"test_key":1000}
