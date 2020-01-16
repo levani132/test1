b7d5747b1c6adaba2af3bd5e2c1b88fb6a1b8382
Igel <gelenidze.irakli@Gmail.com>
2020-01-17 01:29:45 +0400

    a

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET']) 
+def foo():
+    data = {"test":23} #request.json
+    return jsonify(data)
+
+app.run(host="localhost", port=8000)
+
+
+#test = {"test_key":1000}
+#print (jsonify(test)) 
