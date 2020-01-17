54840f743c70cb73a36985c3bace0c03651acf91
David <dvddevadze@gmail.com>
2020-01-17 17:36:18 +0400

    changes with levan

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def submit():
 	v_res= [{"a": 1, "b": 2}, {"a": 4, "b": 6}]
 	if request.method == 'POST':
 		dato = request.json
-		print(dato[0]['ID'])
+		print(dato['ID'])
 		##print(type(dato))
 		connection = psycopg2.connect(user = "vukyhtaqmatqpj",
                               password = "2cf5b5c6b3b7d7e99f02ddc474088225a0015c93996b515194872873f96f65b4",
@@ -21,17 +21,17 @@ def submit():
 
 		cursor = connection.cursor()
 		text = '''INSERT INTO mobile VALUES ((%s), (%s),999);'''
-		cursor.execute(text,(dato[0]['ID'],dato[0]['MODEL']))
+		cursor.execute(text,(dato['ID'],dato['MODEL']))
 		connection.commit()
 		columns = ('ID','MODEL')
-		temp = dato[0]['ID']
+		temp = dato['ID']
 		cursor.execute("select * from mobile where id = (%s)", [temp])
-		result = []
-		for row in cursor.fetchall():
-			result.append(dict(zip(columns,row)))
-		sel = cursor.fetchall()
-		zaza = json.dumps(sel)
-		return json.dumps(result, indent=2)
+		#result = []
+		#for row in cursor.fetchone():
+		#	result.append(dict(zip(columns,row)))
+		sel = cursor.fetchone()
+		#zaza = json.dumps(sel)
+		return json.dumps(sel)
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
