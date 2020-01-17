c7434e168fc1693aa4e55ece515c56532b900fe7
David <dvddevadze@gmail.com>
2020-01-17 12:22:06 +0400

    new

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
-from flask import Flask, request, jsonify
-import os
+from flask import Flask, render_template, request, jsonify
+import json
+import psycopg2
 
 app = Flask(__name__)
-port = int(os.environ.get('PORT', 17995)) 
-@app.route('/', methods=['GET']) 
-def foo():
-    data = {"test":23} #request.json
-    return jsonify(data)
-app.run(host="0.0.0.0", port=5000)
-#app.run( port=port)
 
-#test = {"test_key":1000}
-#print (jsonify(test)) 
+@app.route('/submit', methods = ['POST'])
+def submit():
+	v_res= [{"a": 1, "b": 2}, {"a": 4, "b": 6}]
+	if request.method == 'POST':
+		dato = request.json
+		print(dato[0]['ID'])
+		##print(type(dato))
+		connection = psycopg2.connect(user = "vukyhtaqmatqpj",
+                              password = "2cf5b5c6b3b7d7e99f02ddc474088225a0015c93996b515194872873f96f65b4",
+                              host = "ec2-54-228-237-40.eu-west-1.compute.amazonaws.com",
+                              port = "5432",
+                              database = "dedn9b8htmngg7")
 
+		cursor = connection.cursor()
+		text = '''INSERT INTO mobile VALUES ((%s), (%s),999);'''
+		cursor.execute(text,(dato[0]['ID'],dato[0]['MODEL']))
+		connection.commit()
+		columns = ('ID','MODEL')
+		temp = dato[0]['ID']
+		cursor.execute("select * from mobile where id = (%s)", [temp])
+		result = []
+		for row in cursor.fetchall():
+			result.append(dict(zip(columns,row)))
+		sel = cursor.fetchall()
+		zaza = json.dumps(sel)
+		return json.dumps(result, indent=2)
 
-#
+if __name__ == '__main__':
+	app.run()
