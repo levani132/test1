00ece4d41ba735aecd398db0dc82fa920d9c32d4
David <dvddevadze@gmail.com>
2020-01-22 20:14:58 +0400

    added auth

--- app.py
+++ app.py
@@ -36,7 +36,30 @@ def submit():
 		#sel = cursor.fetchone()
 		#zaza = json.dumps(sel)
 		return json.dumps(result[0])
+@app.route('/auth', methods = ['POST'])
+def auth():
+	if request.method == 'POST':
+		dato = request.json
 
+		connection = psycopg2.connect(user = "vukyhtaqmatqpj",
+                                      password = "2cf5b5c6b3b7d7e99f02ddc474088225a0015c93996b515194872873f96f65b4",
+                                      host = "ec2-54-228-237-40.eu-west-1.compute.amazonaws.com",
+                                      port = "5432",
+                                      database = "dedn9b8htmngg7")
+		cursor = connection.cursor()
+		text = 'select phone_number from parking.users where phone_number = (%s);'
+		cursor.execute(text, [dato['phoneNumber']])
+		sel = cursor.fetchall()
+		encoded = jwt.encode({'phoneNumber': sel[0], 'endDate':str(date.today())}, key, algorithm='HS256')
+		print(str(encoded))
+		cols = ('token',)
+		rows = (encoded.decode("utf-8"),)
+		result = []
+		result.append(dict(zip(cols,rows)))   
+		return json.dumps(result[0])
+
+
+		
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
 	my_port = str(port)
