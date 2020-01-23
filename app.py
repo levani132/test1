0f9ef0766a7be69a106bc12b3b240c59e7a18ae7
David <dvddevadze@gmail.com>
2020-01-23 15:10:13 +0400

    search

--- app.py
+++ app.py
@@ -165,6 +165,32 @@ def scan():
         m = p.match(text)
         return(json.dumps({ 'text': text }))
 
+@app.route('/search', methods = ['POST'])
+@loginRequired
+def search():
+    errorText = None
+    zaza = ['', '']
+    try:        
+        dato = request.json
+        connection = psycopg2.connect(user = "vukyhtaqmatqpj",
+                                    password = "2cf5b5c6b3b7d7e99f02ddc474088225a0015c93996b515194872873f96f65b4",
+                                    host = "ec2-54-228-237-40.eu-west-1.compute.amazonaws.com",
+                                    port = "5432",
+                                    database = "dedn9b8htmngg7")
+        cursor = connection.cursor()
+        text = "select name,phone_number from parking.users where id = (select user_id from parking.cars where car_number = (%s));"
+        cursor.execute(text,[dato['carNumber']])
+        zaza = cursor.fetchone()
+        if zaza == None:
+            raise NameError('მანქანის ნომერი არ მოიძებნა')
+    except Exception as e:
+        errorText = str(e)
+    finally:
+        cursor.close()
+        connection.close()
+        #print(cursor.fetchall())
+        return json.dumps({'name':zaza[0],'phoneNumber':zaza[1],'error': errorText})
+
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
 	my_port = str(port)
