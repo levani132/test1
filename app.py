15c28974613f7ce90a878c88a5b39cea6b2b6e82
David <dvddevadze@gmail.com>
2020-01-23 19:31:46 +0400

    tabs

--- app.py
+++ app.py
@@ -30,21 +30,21 @@ pytesseract.pytesseract.tesseract_cmd = r'/app/.apt/usr/bin/tesseract'
 key = 'parting'
 
 def loginRequired(f):
-	@wraps(f)
-	def decoratedFunction(*args, **kwargs):
-		try:
-			decoded = jwt.decode(request.headers.get('Authorization'), key, algorithms='HS256')
-			datetimeObject = datetime.strptime(decoded['endDate'], '%Y-%m-%d %H:%M:%S.%f')
-			if (datetimeObject + timedelta(days=30)) < datetime.today():
-				raise NameError('Token expired!')
-			return f(*args, **kwargs)
-		except Exception as e:
-			cols = ('error',)
-			rows = (str(e),)
-			result = []
-			result.append(dict(zip(cols,rows)))
-			return json.dumps(result[0])
-	return decoratedFunction
+    @wraps(f)
+    def decoratedFunction(*args, **kwargs):
+        try:
+            decoded = jwt.decode(request.headers.get('Authorization'), key, algorithms='HS256')
+            datetimeObject = datetime.strptime(decoded['endDate'], '%Y-%m-%d %H:%M:%S.%f')
+            if (datetimeObject + timedelta(days=30)) < datetime.today():
+                raise NameError('Token expired!')
+            return f(*args, **kwargs)
+        except Exception as e:
+            cols = ('error',)
+            rows = (str(e),)
+            result = []
+            result.append(dict(zip(cols,rows)))
+            return json.dumps(result[0])
+    return decoratedFunction
 
 
 app = Flask(__name__)
@@ -53,66 +53,66 @@ app = Flask(__name__)
 
 @app.route('/submit', methods = ['POST'])
 def submit():
-	if request.method == 'POST':
-		dato = request.json
-		print('dato', dato)
-		print(dato['ID'])
-		##print(type(dato))
-		connection = psycopg2.connect(user = "vukyhtaqmatqpj",
+    if request.method == 'POST':
+        dato = request.json
+        print('dato', dato)
+        print(dato['ID'])
+        ##print(type(dato))
+        connection = psycopg2.connect(user = "vukyhtaqmatqpj",
                               password = "2cf5b5c6b3b7d7e99f02ddc474088225a0015c93996b515194872873f96f65b4",
                               host = "ec2-54-228-237-40.eu-west-1.compute.amazonaws.com",
                               port = "5432",
                               database = "dedn9b8htmngg7")
 
-		cursor = connection.cursor()
-		text = '''INSERT INTO mobile VALUES ((%s), (%s),999);'''
-		cursor.execute(text,(dato['ID'],dato['MODEL']))
-		connection.commit()
-		columns = ('ID','MODEL')
-		temp = dato['ID']
-		cursor.execute("select * from mobile where id = (%s)", [temp])
-		result = []
-		for row in cursor.fetchall():
-			result.append(dict(zip(columns,row)))
-		#sel = cursor.fetchone()
-		#zaza = json.dumps(sel)
-		return json.dumps(result[0])
+        cursor = connection.cursor()
+        text = '''INSERT INTO mobile VALUES ((%s), (%s),999);'''
+        cursor.execute(text,(dato['ID'],dato['MODEL']))
+        connection.commit()
+        columns = ('ID','MODEL')
+        temp = dato['ID']
+        cursor.execute("select * from mobile where id = (%s)", [temp])
+        result = []
+        for row in cursor.fetchall():
+            result.append(dict(zip(columns,row)))
+        #sel = cursor.fetchone()
+        #zaza = json.dumps(sel)
+        return json.dumps(result[0])
 
 @app.route('/auth', methods = ['POST'])
 def auth():
-	if request.method == 'POST':
-		dato = request.json
+    if request.method == 'POST':
+        dato = request.json
 
-		connection = psycopg2.connect(user = "vukyhtaqmatqpj",
+        connection = psycopg2.connect(user = "vukyhtaqmatqpj",
                                       password = "2cf5b5c6b3b7d7e99f02ddc474088225a0015c93996b515194872873f96f65b4",
                                       host = "ec2-54-228-237-40.eu-west-1.compute.amazonaws.com",
                                       port = "5432",
                                       database = "dedn9b8htmngg7")
-		cursor = connection.cursor()
-		text = 'select phone_number from parking.users where phone_number = (%s);'
-		cursor.execute(text, [dato['phoneNumber']])
-		sel = cursor.fetchall()
-		errorCode = ''
-		if sel == []:
-			errorCode = 'ნომერი არ არის რეგისტრირებული.'
-			encoded = ''
-		else:		
-			encoded = jwt.encode({'phoneNumber': sel[0], 'endDate':str(datetime.today())}, key, algorithm='HS256').decode("utf-8")
-		cols = ('token','error')
-		rows = (encoded,errorCode)
-		result = []
-		result.append(dict(zip(cols,rows))) 
-		return json.dumps(result[0])
+        cursor = connection.cursor()
+        text = 'select phone_number from parking.users where phone_number = (%s);'
+        cursor.execute(text, [dato['phoneNumber']])
+        sel = cursor.fetchall()
+        errorCode = ''
+        if sel == []:
+            errorCode = 'ნომერი არ არის რეგისტრირებული.'
+            encoded = ''
+        else:        
+            encoded = jwt.encode({'phoneNumber': sel[0], 'endDate':str(datetime.today())}, key, algorithm='HS256').decode("utf-8")
+        cols = ('token','error')
+        rows = (encoded,errorCode)
+        result = []
+        result.append(dict(zip(cols,rows))) 
+        return json.dumps(result[0])
 
 @app.route('/getLoginStatus', methods = ['POST'])
 @loginRequired
 def getLoginStatus():
-	if request.method == 'POST':
-		cols = ('isAuthenticated',)
-		rows = (True,)
-		result = []
-		result.append(dict(zip(cols,rows)))
-		return json.dumps(result[0]) 
+    if request.method == 'POST':
+        cols = ('isAuthenticated',)
+        rows = (True,)
+        result = []
+        result.append(dict(zip(cols,rows)))
+        return json.dumps(result[0]) 
 
 @app.route('/register', methods = ['POST'])
 def register():
@@ -174,12 +174,12 @@ def scan():
                                     port = "5432",
                                     database = "dedn9b8htmngg7")
         cursor = connection.cursor()
-		text = '''INSERT INTO parking.logs (data) VALUES ((%s));'''
-		cursor.execute(text,(data['base64']))
-		connection.commit()
-		# file1 = open("/app/log.txt","w")
-		# file1.write( text +"\n"+m)
-		# file1.close()
+        text = '''INSERT INTO parking.logs (data) VALUES ((%s));'''
+        cursor.execute(text,(data['base64']))
+        connection.commit()
+        # file1 = open("/app/log.txt","w")
+        # file1.write( text +"\n"+m)
+        # file1.close()
         print(im2)
         with open('/app/log.txt', 'w') as file:
             file.write(text+"-text \n")
@@ -215,6 +215,6 @@ def search():
         return (json.dumps({'name':zaza[0],'phoneNumber':zaza[1],'error': errorText}))
         
 if __name__ == '__main__':
-	port = int(os.environ.get('PORT', 5000))
-	my_port = str(port)
-	app.run(host='0.0.0.0', port=my_port)
+    port = int(os.environ.get('PORT', 5000))
+    my_port = str(port)
+    app.run(host='0.0.0.0', port=my_port)
